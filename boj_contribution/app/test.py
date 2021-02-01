8ed9043ca93b319da79b201d4e58f4c8f1d468eb
jaeesu <50989437+jaeesu@users.noreply.github.com>
2021-02-01 21:35:03 +0900

    Delete boj_contribution/app directory

--- boj_contribution/app/test.py
+++
@@ -1,8 +0,0 @@
-from request import get_message
-from service.SettingResponse import send_request, Response_repos
-
-if __name__=='__main__':
-    h_req = get_message()
-    h_req.repos('jaeesu')
-    print(Response_repos(send_request(h_req)))
-
